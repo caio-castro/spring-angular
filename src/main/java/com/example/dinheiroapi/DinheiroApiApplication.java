d2c67a71a7452f3a9af3c0b095ef894092b192d4
caio castro <caio-rb@hotmail.com>
2021-03-14 18:31:32 -0300

    Atualizando o master

---
+++ src/main/java/com/example/dinheiroapi/DinheiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dinheiroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinheiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinheiroApiApplication.class, args);
+	}
+
+}
