b7da4b7f1db8bab579310aadc142bf6781d63d47
caio castro <caio-rb@hotmail.com>
2021-04-03 18:57:01 -0300

    Validando valores inválidos com Bean Validation

--- src/main/java/com/example/dinheiroapi/exceptionhandler/DinheiroExceptionHandler.java
+++ src/main/java/com/example/dinheiroapi/exceptionhandler/DinheiroExceptionHandler.java
@@ -1,4 +1,8 @@
 package com.example.dinheiroapi.exceptionhandler;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -6,6 +10,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -18,11 +25,33 @@ public class DinheiroExceptionHandler extends ResponseEntityExceptionHandler  {
 	
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		HttpHeaders headers, HttpStatus status, WebRequest request) {
         String mensagemUsuario = messageSouce.getMessage("messagem.ivalida",null, LocaleContextHolder.getLocale());
         String mensagemDesenvolvedor = ex.getCause().toString();
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario,mensagemDesenvolvedor));
         
-		return handleExceptionInternal(ex, new Erro(mensagemUsuario,mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		    List<Erro> erros = criatListaErros(ex.getBindingResult());
+		    return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> criatListaErros(BindingResult bindingResult){
+		List<Erro> erros = new ArrayList<>();
+		
+		for(FieldError fieldError :  bindingResult.getFieldErrors()) {
+			String mensagemUsuario = messageSouce.getMessage(fieldError, LocaleContextHolder.getLocale());
+	        String mensagemDesenvolvedor = fieldError.toString(); 
+			erros.add(new Erro(mensagemUsuario,mensagemDesenvolvedor));
+		}
+		
+		return erros;
 	}
 	
 	public static class Erro {
--- src/main/java/com/example/dinheiroapi/resource/CategoriaResource.java
+++ src/main/java/com/example/dinheiroapi/resource/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -34,7 +35,7 @@ public class CategoriaResource {
 	@PostMapping
 	// Colocando o status 201 quando é feita uma inclusão post
 	//@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		 Categoria categoriaSalva = categoriaRepository.save(categoria);
 		 
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
